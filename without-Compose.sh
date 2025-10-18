10d35bdb45526fb99891265c8ed8dd12a19889df
Sandro <info@saninfo.de>
2025-10-18 17:28:29 +0200

    beauty up prototype script

--- without-Compose.sh
+++ without-Compose.sh
@@ -1,5 +1,7 @@
-#  Without Compose if Docker-compose wont work.
-# docker stop master1 master2 bbdd web sonda influx grafana
+# Without-Compose.sh Prototype Script, still in development as long as Docker-compose issue "ContainerConfig" is not solved
+
+# clean docker env from old containers if existing and build images & NW
+docker stop master1 master2 bbdd web sonda influx grafana
 docker container rm master1 master2 bbdd web sonda influx grafana
 
 docker network create --subnet=172.18.0.0/16 icinganet
@@ -7,22 +9,22 @@ docker build ./master/. -t sandrosano/icingamaster0-1
 docker build ./web/. -t sandrosano/icingaweb0-1
 docker build ./sonda/. -t sandrosano/icingasonda0-1
 
+
+# Master Containers Run & Config
 docker run -d --name master1 --net icinganet --ip 172.18.0.10 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro sandrosano/icingamaster0-1   
 echo 'unpriviledged alternative might be avilable but i dont care about sec NOW in dev mode. generally , systemD in unprivileged docker is difficult to get working properly.'
-# docker run -d --name master2 --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro jrei/systemd-debian /install.sh
 echo "Master1 config"
 docker exec master1 /bin/bash -c 'sh /config.sh'
-
 
 docker run -d --name master2 --net icinganet --ip 172.18.0.11 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro sandrosano/icingamaster0-1  
 echo "Master2 config"
 docker exec master2 /bin/bash -c 'sh /config.sh'
 #
 #
-read -p "Everything okay? Lets Continue, press enter key.." REPLY
-#
-#
-echo 'secure Password management will be introduced later when everything works.'
+read -p "Everything okay? Lets Continue with Database setup, press enter key.." REPLY
+# DATABASE Container Run & Config
+# TODO introduce secure password management either here or directly in orchestrated setup
+echo 'secure password management will be introduced later when everything works.'
 docker volume create bbdd-var-lib-mysql
 docker run --name bbdd -v bbdd-var-lib-mysql:/var/lib/mysql  --net icinganet --ip 172.18.0.13 -e MYSQL_ROOT_PASSWORD=myrootpw -d mysql:8
 echo "bbdd user"
@@ -32,10 +34,9 @@ docker exec -it bbdd /bin/bash -c 'mysql -u root -pmyrootpw  < /user.sql'
 docker exec -it bbdd /bin/bash -c 'mysql -u root -pmyrootpw -e "SET PERSIST require_secure_transport=OFF;"'
 docker exec -it bbdd /bin/bash -c 'mysql -u root -pmyrootpw -e "set global log_bin_trust_function_creators=1"' 
 #
-#
-read -p "Everything okay? Lets Continue, press enter key.." REPLY
-#
-echo "Master-to-DB Init, table schema"
+# Master-to-DB Init, table schema
+read -p "Everything okay? Lets Continue with Master-to-DB Init, setting table schema. press enter key.." REPLY
+# TODO SSL config for DB & WEB connections, enforce SSL
 echo 'Dev Mode: NO SSL configuren for DB connection: --skip-ssl'
 docker exec -it master1 /bin/bash -c "mysql -u icingadb -p icingadb --password=icing12bns98h4 --host=172.18.0.13 --skip-ssl </usr/share/icingadb/schema/mysql/schema.sql"
 docker exec master1 /bin/bash -c 'systemctl enable --now icingadb'
@@ -44,13 +45,11 @@ docker exec master1 /bin/bash -c 'systemctl restart icingadb'
 docker exec master2 /bin/bash -c 'systemctl restart icingadb'
 #
 #
-read -p "Everything okay? Lets Continue, press enter.."   REPLY
-#
-#
+read -p "Everything okay? Lets Continue with the MASTER NODE WIZARDS, Zones and Api configs, press enter.."   REPLY
+# TODO automate node wizard via "node setup"- based script
 echo "Master1 Wizard"
 docker exec -it master1 /bin/bash -c "icinga2 node wizard"
 docker exec -it master1 /bin/bash -c 'systemctl restart icinga2'
-
 docker exec -it master1 /bin/bash -c "sed -i '6c\object Endpoint \"master1\" {    host = \"172.18.0.10\" } ' /etc/icinga2/zones.conf"
 docker exec -it master1 /bin/bash -c "sed -i '7c\object Endpoint \"master2\" {    host = \"172.18.0.11\" } ' /etc/icinga2/zones.conf"
 docker exec -it master1 /bin/bash -c "sed -i '10c\        endpoints = [\"master1\",\"master2\"]  ' /etc/icinga2/zones.conf"
@@ -63,7 +62,7 @@ docker exec -it master1 /bin/bash -c 'vi /etc/icinga2/features-enabled/api.conf'
 docker exec -it master1 /bin/bash -c 'systemctl restart icinga2'
 docker exec -it master1 /bin/bash -c 'systemctl restart icinga2.service'
 
-echo "Master2 Wiszard"
+echo "Master2 Wizard"
 docker exec -it master2 /bin/bash -c "icinga2 node wizard"
 docker exec -it master2 /bin/bash -c 'systemctl restart icinga2'
 docker exec -it master2 /bin/bash -c "sed -i '6c\object Endpoint \"master2\" {    host = \"172.18.0.11\" } ' /etc/icinga2/zones.conf"
@@ -78,32 +77,31 @@ docker exec -it master2 /bin/bash -c 'vi /etc/icinga2/constants.conf'
 docker exec -it master2 /bin/bash -c 'systemctl restart icinga2.service'
 #
 #
-read -p "Everything okay? Lets Continue, press enter key.."  REPLY
+read -p "Everything okay? Lets Continue with the Web config, press enter key.."  REPLY
 #
 #
 echo "Web config"
 docker run -d --name web --net icinganet --ip 172.18.0.12 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro sandrosano/icingaweb0-1   
-
-
 docker exec -it web /bin/bash -c 'icingacli setup token create' | tee ./web/token.txt
 docker exec -it web /bin/bash -c 'systemctl restart icinga-director'
+echo "Token for Web Setup:"
+echo ./web/token.txt
+echo -p "Go to 172.18.0.12/icingaweb2/setup and follow the wizard, inserting the tokey you see above" REPLY
 #
 #
-#
-read -p "Everything okay? Lets Continue, press enter key.."  REPLY
-#
-#
-#
-echo -p "lets compare config across masters.." REPLY
+read -p "Lets compare & assert correct config across masters.. "  REPLY
 docker exec -it master1 /bin/bash -c 'icinga2 daemon -C'
 docker exec -it master2 /bin/bash -c 'icinga2 daemon -C'
 #
 #
 #
-echo -p " lets config the sonde like the following" REPLY
-cat ./sonda/tutorial-Node-Wizard.txt
+echo -p " lets config the sonde. Do you need info first? (y= prints info)" REPLY
+if [ REPLY == "y" ];
+    then
+        cat ./sonda/tutorial-Node-Wizard.txt
+    fi
 docker exec master1 /bin/bash -c "icinga2 pki ticket --cn 'sonda1'" | tee ./sonda/sonda1.token
-echo "token for sonda1"
+echo "token for sonda1:"
 cat ./sonda/sonda1.token
 echo -p " using for sonda1 upper token ( for other sondas please generate new )" REPLY
 docker run -d --name sonda1 --net icinganet --ip 172.18.0.16 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro sandrosano/icingasonda0-1   
@@ -115,5 +113,4 @@ docker exec -it sonda1 /bin/bash -c 'systemctl restart icinga2'
 echo -p "if you want to backup web and bbdd and mysql data as state is nonconsistent after UI manual input.. EXECUTE Manually FOLLOWING: sudo tar -cvf ./images/bbdd-var-lib-mysql_setted-L114 /var/lib/docker/volumes/bbdd-var-lib-mysql/_data && docker export bbdd > ./images/bbdd_setted-L114.tar && docker export web > web_setted-L114.tar   " REPLY
 
 
-echo -p "Now lets check thecentreon plugins" REPLY
-docker exec -it sonda1 /bin/bash -c '/usr/lib/nagios/plugins/centreon-plugins/src/centreon_plugins.pl --plugin=os::linux::snmp::plugin --list-mode'
+echo -p "Enjoy your Icinga2 Installation" REPLY
